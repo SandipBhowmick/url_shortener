187a5e6036853ec6e961b8c729f920c94ad5721e
Sandip <bhowmick.sandip@gmail.com>
2017-07-30 04:23:36 +0530

    add request.url to index

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170729161620) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "urls", force: :cascade do |t|
     t.string   "full_url"
     t.string   "shortened_url"
